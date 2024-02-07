72726b2d4c8082350233200beb5776cf0657962f
webdesignervinayak <71724290+webdesignervinayak@users.noreply.github.com>
2024-02-08 02:59:50 +0530

    adding scroll buttons

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -21,18 +21,44 @@ const HourlyForecast = () => {
         getHourlyForecastDetails();
     },[locationDetails])
 
+    const scrollLeft = () => {
+        const container = document.getElementById('hourly-forecast-container');
+        if (container) {
+            container.scrollBy({
+                left: -200,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        const container = document.getElementById('hourly-forecast-container');
+        if (container) {
+            container.scrollBy({
+                left: 200,
+                behavior: 'smooth'
+            });
+        }
+    };
+
 
     //console.log(hourlyDetails);
 
   return (
     <div className='w-[48rem] px-4 text-white'>
+        <div className=' flex justify-between w-[46rem]'>
         <h1 className='mt-4 p-2 text-slate-700 font-bold text-2xl'>Hourly Forecast</h1>
-        <div className='flex overflow-x-scroll no-scrollbar'>
-            <div className='flex'>
+        <div className='flex mt-4'>
+                <button className='p-2 mx-2 bg-slate-600 bg-opacity-60 text-white rounded-full' onClick={scrollLeft}>{'<'}</button>
+                <button className='p-2 mx-2 bg-slate-600 bg-opacity-60 text-white rounded-full' onClick={scrollRight}>{'>'}</button>
+        </div>
+        </div>
+        <div className='flex overflow-x-scroll no-scrollbar' id="hourly-forecast-container">
+        <div className='flex'>
         {
             hourlyDetails.list.map( (l) => <HourlyForeCastBox key={l.dt} data={l}/> )       
         } 
-            </div>
+        </div>
 
         </div>   
     </div>
